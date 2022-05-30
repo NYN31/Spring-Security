05466022e092fb339b9e5fdc9890de2e72579af2
NYN31 <noyoncse310197@gmail.com>
2022-05-30 20:26:39 +0600

    Lesson-5: edited few files, no feature added

--- src/main/java/com/springsecurity/demo/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -6,9 +6,14 @@ public class SecurityWebApplicationInitializer
 						extends AbstractSecurityWebApplicationInitializer {
 
 	/***
-	 protected AbstractSecurityWebApplicationInitializer() Creates a new instance that assumes
-	 the Spring Security configuration is loaded by some other means than this class.
-	 For example, a user might create a ContextLoaderListener using a subclass of
-	 AbstractContextLoaderInitializer .
+	 * protected AbstractSecurityWebApplicationInitializer() Creates a new instance that assumes
+	 * the Spring Security configuration is loaded by some other means than this class.
+	 * For example, a user might create a ContextLoaderListener using a subclass of
+	 * AbstractContextLoaderInitializer .
+	 *
+	 * Main purpose to register(triggering, enabling) the Spring Security Filters
+	 *
+	 * Just need to extend AbstractSecurityWebApplicationInitializer, no need to code, no
+	 * need to override code, no need to override methods, nothing to configure
 	 ***/
 }
